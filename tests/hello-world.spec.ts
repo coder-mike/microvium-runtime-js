eb4c2fe0e603f71a38b1c7a254c7bd0f260175f1
Michael Hunter <elec.mike@gmail.com>
2023-03-09 07:35:40 +1100

    Tests for fmod and fpow

--- tests/hello-world.spec.ts
+++ tests/hello-world.spec.ts
@@ -2,6 +2,26 @@ import Runtime from '../src/index';
 import assert from 'assert/strict';
 import { Microvium, addDefaultGlobals } from 'microvium';
 
+const basicValues: any[] = [
+  undefined,
+  null,
+  true,
+  false,
+  42,
+  -42,
+  420000,
+  -420000,
+  1.5,
+  -0,
+  NaN,
+  Infinity,
+  -Infinity,
+  '__proto__',
+  'length',
+  'any string',
+  '',
+]
+
 test('hello-world', async () => {
   const source = `
     const print = vmImport(1);
@@ -24,9 +44,12 @@ test('hello-world', async () => {
   main();
 
   assert.equal(print, 'hello, world');
-})
+});
 
 test('pass basic values', async () => {
+  // This tests the passing of basic values in both directions across the
+  // boundary and preserving in Microvium memory.
+
   const source = `
     let x;
     vmExport(1, () => x); // get
@@ -43,26 +66,43 @@ test('pass basic values', async () => {
     assert(Object.is(get(), value)); // Covers -0 as well
   }
 
-  testValue(undefined);
-  testValue(null);
-  testValue(true);
-  testValue(false);
-  testValue(42);
-  testValue(-42);
-  testValue(420000);
-  testValue(-420000);
-  testValue(1.5);
-  testValue(-0);
-  testValue(NaN);
-  testValue(Infinity);
-  testValue(-Infinity);
-  testValue('__proto__');
-  testValue('length');
-  testValue('any string');
-  testValue('');
-})
+  for (const value of basicValues) {
+    testValue(value);
+  }
+});
 
-// TODO: Test function call arguments and return value both directions
+test('fmod and pow', async () => {
+  // The operators fmod and pow are outsourced from the VM to the host. This
+  // tests that they work correctly
+
+  const source = `
+    vmExport(1, (x, y) => x % y); // fmod
+    vmExport(2, (x, y) => x ** y); // pow
+  `;
+
+  const snapshot = compile(source);
+  const vm = await Runtime.restore(snapshot, {});
+  const { [1]: fmod, [2]: pow } = vm.exports;
+
+  assert.equal(fmod(0, 2), 0 % 2);
+  assert.equal(fmod(10, 2), 10 % 2);
+  assert.equal(fmod(10.5, 2), 10.5 % 2);
+  assert.equal(fmod(-10.5, 2), -10.5 % 2);
+  assert.equal(fmod(10.5, 1.5), 10.5 % 1.5);
+  assert.equal(fmod(10.5, -1.5), 10.5 % -1.5);
+  assert.equal(fmod(-10.5, 1.5), -10.5 % 1.5);
+  assert.equal(fmod(-10.5, -1.5), -10.5 % -1.5);
+
+  assert.equal(pow(0, 2), 0 ** 2);
+  assert.equal(pow(10, 2), 10 ** 2);
+  assert.equal(pow(10.5, 2), 10.5 ** 2);
+  assert.equal(pow(-10.5, 2), (-10.5) ** 2);
+  assert.equal(pow(10.5, 1.5), 10.5 ** 1.5);
+  assert.equal(pow(10.5, -1.5), 10.5 ** -1.5);
+  assert.equal(pow(-10.5, 1.5), (-10.5) ** 1.5);
+  assert.equal(pow(-10.5, -1.5), (-10.5) ** -1.5);
+});
+
 // TODO: Test fmod and pow
 // TODO: Basic performance test
 
